13f2366017c0808070bc1d708ca422afe6852c87
Sirapob Phasut <omegaooh@gmail.com>
2024-11-14 09:21:25 +0700

    Fix minor typo and mistake

--- backend/fire_station/models.py
+++ backend/fire_station/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from choice_enum import DayOfWeek, FireFighterRank, FireFighterRole, IncidentType, Gender
+from .choice_enum import DayOfWeek, FireFighterRank, FireFighterRole, IncidentType, Gender
 
 
-class Station(models.Models):
+class Station(models.Model):
     """Station Information. Capacity and Address"""
-    address = models.CharField()
+    address = models.CharField(max_length=255)
     staff_capacity = models.IntegerField()
     fire_engine_capacity = models.IntegerField()
 
@@ -24,10 +24,10 @@ class Staff(models.Model):
     """Fire Station Staff."""
     full_name = models.CharField(max_length=100)
     dob = models.DateField()
-    gender = models.CharField(choice=Gender)
+    gender = models.CharField(choices=Gender, max_length=6)
     position = models.CharField(max_length=100)
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
-    shift = models.ManyToManyField(Shift, on_delete=models.CASCADE)
+    shift = models.ManyToManyField(Shift)
 
     def __str__(self):
         return f"{self.full_name}"
@@ -58,7 +58,7 @@ class FireEngine(models.Model):
     engine_number = models.IntegerField()
     model = models.CharField(max_length=100)
     license_plate = models.CharField(max_length=10)
-    equipments = models.ManyToManyField(Equipment, on_delete=models.CASCADE)
+    equipments = models.ManyToManyField(Equipment)
 
     def __str__(self):
         return f"{self.model} license plate: {self.license_plate}"
@@ -67,8 +67,8 @@ class FireEngine(models.Model):
 class Dispatch(models.Model):
     """Common Info of dispatch"""
     incident = models.IntegerField(choices=IncidentType)
-    reported_time = models.DateTimeField(default=timezone.now())
-    notified_time = models.DateTimeField(default=timezone.now())
+    reported_time = models.DateTimeField(default=timezone.now)
+    notified_time = models.DateTimeField(default=timezone.now)
     address = models.CharField(max_length=250)
 
     def __str__(self):
@@ -78,9 +78,9 @@ class Dispatch(models.Model):
 class DispatchAssignment:
     """Fire Station records of resource used for the response"""
     dispatch = models.ForeignKey(Dispatch, on_delete=models.CASCADE)
-    fire_engine = models.ManyToManyField(FireEngine, on_delete=models.CASCADE)
-    equipment_used = models.ManyToManyField(Equipment, on_delete=models.CASCADE)
-    fire_fighter = models.ManyToManyField(FireFighter, on_delete=models.CASCADE)
+    fire_engine = models.ManyToManyField(FireEngine)
+    equipment_used = models.ManyToManyField(Equipment)
+    fire_fighter = models.ManyToManyField(FireFighter)
     time_back_at_station = models.DateTimeField(default=timezone.now())
 
     def __str__(self):
