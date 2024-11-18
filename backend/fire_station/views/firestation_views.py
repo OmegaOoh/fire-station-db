494f5c30d944a961a21d9cc22c1cc7b89f433407
Pasu Sangiemsin <opxz7148@gmail.com>
2024-11-18 20:40:40 +0700

    Add station name field to station create page

---
+++ backend/fire_station/views/firestation_views.py
@@ -0,0 +1,23 @@
+from typing import Any
+from rest_framework import decorators, response, generics, mixins
+from django.http import HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from .. import serializer
+from .. import models
+
+
+class FireStationView(
+    generics.GenericAPIView,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin
+):
+
+    queryset = models.Station.objects.all()
+    serializer_class = serializer.FireStationSerializer
+
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
+        return self.list(request, *args, **kwargs)
+
+    @csrf_exempt
+    def post(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
+        return self.create(request, args, kwargs)
