85f6353fe84cd269d05cf34af85b27464f984018
Pasu Sangiemsin <opxz7148@gmail.com>
2024-11-20 12:27:46 +0700

    Add import inside init file

--- backend/fire_station/views/__init__.py
+++ backend/fire_station/views/__init__.py
@@ -1,3 +1,4 @@
+from .firestation_views import FireStationView, FireStationDetailView
 from .views import test_view
 from .equipments import EquipmentsView
-from .firestation_views import FireStationView, FireStationDetailView
+
