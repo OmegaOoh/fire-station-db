4e508bfbcfc2676563ea60d3dff7e3485ad2e1a1
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-21 11:38:59 +0700

    Improve docstrings and comments

--- backend/fire_station/views/fire_engine.py
+++ backend/fire_station/views/fire_engine.py
@@ -40,6 +40,7 @@ class FireEngineView(
         if self.get_queryset().filter(id=fire_engine_id).exists():
             return self.update(request, *args, **kwargs)
         else:
+            # Check if number of fire engines exceed the capacity
             res = self.__check_fire_engine_capacity(request, *args, **kwargs)
             if res:
                 return res
@@ -51,9 +52,11 @@ class FireEngineView(
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def put(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
+        """Handle put request by updating fire engines and return list of fire engines."""
         return self.update(request, *args, **kwargs)
 
     def update(self, request: HttpRequest, *args: Any, **kwargs: Any) -> response.Response:
+        """Update fire engines according to the id data and return list of fire engines."""
         fire_engine_id = request.data.get('id')
         instance = self.get_queryset().get(id=fire_engine_id)
         serializer = self.get_serializer(instance, data=request.data, partial=True)
@@ -64,6 +67,7 @@ class FireEngineView(
         return response.Response(serializer.data, status=status.HTTP_200_OK)
 
     def __check_fire_engine_capacity(self, request, *args, **kwargs):
+        """Check if number of fire engines exceed the capacity."""
         fire_station_id = request.data.get('station')
         fire_station = models.Station.objects.get(id=fire_station_id)
         if models.FireEngine.objects.filter(
