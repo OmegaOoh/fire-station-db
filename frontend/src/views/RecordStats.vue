59768ff9bccfa1f6556161f981f00bc757daf7df
Sirapob Phasut <omegaooh@gmail.com>
2024-11-21 11:25:19 +0700

    Add simple data to Home page

--- frontend/src/views/RecordStats.vue
+++ frontend/src/views/RecordStats.vue
@@ -1,5 +1,39 @@
 <template>
-    <h2>
-        Record Stats
-    </h2>
+    <div class="pt-4 px-4">
+        <h1 class="text-4xl font-bold mb-6">
+            Record Stats
+        </h1>
+        <div class="stats stats-vertical lg:stats-horizontal shadow w-full">
+            <div class="stat">
+                <div class="stat-title font-semibold"># of Response</div>
+                <div class="stat-value text-secondary">{{ data.incident }}</div>
+                <div class="stat-desc">Jan 1st - Feb 1st</div>
+            </div>
+
+            <div class="stat">
+                <div class="stat-title font-semibold">Average Time Resolved</div>
+                <div class="stat-value text-primary">{{ data.avg_resolve }}</div>
+                <div class="stat-desc">Minutes</div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const data = ref({
+    incident: 210,
+    avg_resolve: 3.15,
+    incident_type: [
+        'Vehicle Fire',
+        'Search and Rescue',
+        'Structure Fire',
+        'HAZMAT Fire',
+    ],
+    incident_nums: [120, 20, 5, 1]
+})
+
+// const fetchData = async() => { ADD CODE TO FETCH DATA HERE }
+
+</script>
