a7beef43b0ee0a2f22774009ae88a0f209f96372
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-21 11:36:22 +0700

    Add URL for FireEngine view

--- backend/fire_station/urls.py
+++ backend/fire_station/urls.py
@@ -6,4 +6,5 @@ app_name = "fire-station"
 urlpatterns = [
     path("check", views.test_view, name="test"),
     path("equipments", views.EquipmentsView.as_view(), name="equipments"),
+    path("fire-engine", views.FireEngineView.as_view(), name="fire-engine"),
 ]
--- backend/fire_station/views/__init__.py
+++ backend/fire_station/views/__init__.py
@@ -1,4 +1,5 @@
 from .views import test_view
 from .equipments import EquipmentsView
+from .fire_engine import FireEngineView
 from .firestation_views import FireStationView
 
