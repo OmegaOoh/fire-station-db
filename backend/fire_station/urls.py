bade75cd8a5859428c80d6299cfd79fe967116e0
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-11-25 21:52:47 +0700

    Add URL to dispatch detail view

--- backend/fire_station/urls.py
+++ backend/fire_station/urls.py
@@ -7,8 +7,8 @@ app_name = "fire-station"
 urlpatterns = [
     path("", views.FireStationView.as_view(), name="station-list"),
     path("<int:pk>/", views.FireStationDetailView.as_view(), name="station-list"),
-    path("choice/", get_choice, name="station-list")
-    path("check", views.test_view, name="test"),
-    path("dispatch", views.DispatchView.as_view(), name="dispatch"),
-    path("dispatch-aggregate", views.DispatchAggregate.as_view(), name="dispatch-aggregate"),
+    path("choice/", get_choice, name="station-list"),
+    path("dispatch/", views.DispatchListView.as_view(), name="dispatch"),
+    path("dispatch/<int:pk>/", views.DispatchDetailView.as_view(), name="dispatch"),
+    path("dispatch-aggregate/", views.DispatchAggregate.as_view(), name="dispatch-aggregate"),
 ]
--- backend/fire_station/views/__init__.py
+++ backend/fire_station/views/__init__.py
@@ -1,4 +1,4 @@
 from .firestation_views import FireStationView, FireStationDetailView
 from .equipments import EquipmentsView
 from .firestation_views import FireStationView
-from .dispatch import DispatchView, DispatchAggregate
+from .dispatch import DispatchListView, DispatchDetailView, DispatchAggregate
